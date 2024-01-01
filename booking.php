3b26bfd443b0da6b0ba56109c4731d69c67e41a5
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2024-01-01 23:22:14 +0100

    Add booking form

---
+++ booking.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book a Room</title>
+</head>
+<body>
+    <h2>Book a Room Now</h2>
+    
+    <form action="processBooking.php" method="post">
+        <label for="name">Your first name:</label>
+        <input type="text" name="name" required><br>
+
+        <label for="name">Your last name:</label>
+        <input type="text" name="name" required><br>
+        
+        <label for="email">Email:</label>
+        <input type="email" name="email" required><br>
+        
+        <label for="checkin">Check-in Date:</label>
+        <input type="date" name="checkin" required><br>
+        
+        <label for="checkout">Check-out Date:</label>
+        <input type="date" name="checkout" required><br>
+        
+        <input type="submit" value="Book Now">
+    </form>
+</body>
+</html>
